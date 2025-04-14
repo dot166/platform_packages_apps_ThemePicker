82298981a4a3fbd6f166c3fa3af48c21d8ea3a08
._______166 <retromac9x2@gmail.com>
2025-04-14 15:53:49 +0100

    allow ThemeEngine to add itself to themepicker
    this is a different method to waht was used in 15 due to me wanting
    themeengine to be updated outside of jOS and the old way would not allow
    that

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -14,6 +14,7 @@ import com.android.customization.model.themedicon.ThemedIconSwitchProvider;
 import com.android.customization.model.themedicon.domain.interactor.ThemedIconInteractor;
 import com.android.customization.model.themedicon.domain.interactor.ThemedIconSnapshotRestorer;
 import com.android.customization.module.logging.ThemesUserEventLogger;
+import com.android.customization.module.te.ThemeEngineSectionController;
 import com.android.customization.picker.clock.ui.view.ClockViewFactory;
 import com.android.customization.picker.clock.ui.viewmodel.ClockCarouselViewModel;
 import com.android.customization.picker.color.domain.interactor.ColorPickerInteractor;
@@ -190,6 +191,8 @@ public final class DefaultCustomizationSections implements CustomizationSections
                                         .get(NotificationSectionViewModel.class),
                                 lifecycleOwner));
 
+                sectionControllers.add(new ThemeEngineSectionController());
+
                 // More settings section.
                 sectionControllers.add(new MoreSettingsSectionController());
                 break;
@@ -211,6 +214,8 @@ public final class DefaultCustomizationSections implements CustomizationSections
                                     mColorContrastSectionViewModelFactory)
                                     .get(ColorContrastSectionViewModel.class), lifecycleOwner));
                 }
+
+                sectionControllers.add(new ThemeEngineSectionController());
                 // App grid section.
                 sectionControllers.add(
                         new GridSectionController(
---
+++ src/com/android/customization/module/te/ThemeEngineSectionView.java
@@ -0,0 +1,12 @@
+package com.android.customization.module.te;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.wallpaper.picker.SectionView;
+
+public final class ThemeEngineSectionView extends SectionView {
+    public ThemeEngineSectionView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+}
